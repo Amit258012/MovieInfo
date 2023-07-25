311d7072b920d7c79d34a441b61ed9b946fe94e1
Amit258012 <amit258012@gmail.com>
2023-07-25 19:11:09 +0530

    redux toolkit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import "./App.css";
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+// import fetchDataFromApi from "./utils/fetchDataFromApi";
+import fetchDataFromApi from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
-	return <>App</>;
+	const dispatch = useDispatch();
+	const { url } = useSelector((state) => state.home);
+	console.log(url);
+	useEffect(() => {
+		apiTesting();
+	}, []);
+	const apiTesting = () => {
+		fetchDataFromApi("/movie/popular").then((response) => {
+			console.log(response);
+			dispatch(getApiConfiguration(response));
+		});
+	};
+	return (
+		<div className="App">
+			App
+			{url?.total_pages}
+		</div>
+	);
 }
 
 export default App;
