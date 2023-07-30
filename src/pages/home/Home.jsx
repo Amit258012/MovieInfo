6cdfa3dff996da9325a77c726b1cf8e9fb464ff1
Amit258012 <amit258012@gmail.com>
2023-07-30 21:10:09 +0530

    Switch tab component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 const Home = () => {
 	return (
 		<div className="heroPage">
 			<HeroBanner />
+			<Trending />
 			<div style={{ height: 1000 }}></div>
 		</div>
 	);
