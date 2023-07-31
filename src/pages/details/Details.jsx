c9cd65eed879decf84c6020cbc4c49031472158d
Amit258012 <amit258012@gmail.com>
2023-07-31 15:35:09 +0530

    Cast details fetched

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,6 +2,7 @@ import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "../cast/Cast";
 const Details = () => {
 	const { mediaType, id } = useParams();
 	const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -11,6 +12,7 @@ const Details = () => {
 	return (
 		<div>
 			<DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+			<Cast data={credits?.cast} loading={creditsLoading} />
 		</div>
 	);
 };
