fef442a678545360c37f2d7dd2a611b9fdf821b8
Amit258012 <amit258012@gmail.com>
2023-07-31 15:53:54 +0530

    Official Videos section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "../cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
 const Details = () => {
 	const { mediaType, id } = useParams();
 	const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -13,6 +14,7 @@ const Details = () => {
 		<div>
 			<DetailsBanner video={data?.results[0]} crew={credits?.crew} />
 			<Cast data={credits?.cast} loading={creditsLoading} />
+			<VideosSection data={data} loading={loading} />
 		</div>
 	);
 };
